ab14c800c3b2be5645528b63dcf6f64436451ca4
123 <1064562879@qq.com>
2020-06-05 11:39:02 +0800

    初始化

---
+++ TomatoOC/TomatoOC/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TomatoOC
+//
+//  Created by 123 on 2020/6/5.
+//  Copyright © 2020 master. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ TomatoOC/TomatoOC/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  TomatoOC
+//
+//  Created by 123 on 2020/6/5.
+//  Copyright © 2020 master. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
